4e49f42a856f5b0c6a8a7649fdbc7dfcdf1e44cf
Arseniy Khvorov <khvorov45@gmail.com>
2023-01-15 18:12:10 +1100

    move init to cc1main

--- clang_src/clang_tools_driver_cc1_main.cpp
+++ clang_src/clang_tools_driver_cc1_main.cpp
@@ -1,16 +1,8 @@
-//===-- cc1_main.cpp - Clang CC1 Compiler Frontend ------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-//
-// This is the entry point to the clang -cc1 functionality, which implements the
-// core compiler functionality along with a number of additional tools for
-// demonstration and testing purposes.
-//
-//===----------------------------------------------------------------------===//
+#include "llvm_lib_Target_X86_TargetInfo_X86TargetInfo.h"
+#include "llvm_lib_Target_X86_X86TargetMachine.h"
+#include "llvm_lib_Target_X86_X86.h"
+
+#include "llvm_include_llvm_InitializePasses.h"
 
 #include "clang_include_clang_Basic_TargetOptions.h"
 #include "clang_include_clang_CodeGen_ObjectFilePCHContainerOperations.h"
@@ -186,13 +178,64 @@ PrintSupportedCPUs(std::string TargetStr) {
     return 0;
 }
 
+static bool
+LLVMX8664MatchProc(llvm::Triple::ArchType archType) {
+    bool result = archType == llvm::Triple::x86_64;
+    return result;
+}
+
+static llvm::TargetMachine*
+LLVMX86TargetMachineProc(const llvm::Target& T, const llvm::Triple& TT, llvm::StringRef CPU, llvm::StringRef FS, const llvm::TargetOptions& Options, std::optional<llvm::Reloc::Model> RM, std::optional<llvm::CodeModel::Model> CM, llvm::CodeGenOpt::Level OL, bool JIT) {
+    return new llvm::X86TargetMachine(T, TT, CPU, FS, Options, RM, CM, OL, JIT);
+}
+
 int
 cc1_main(int argc, char** argv) {
+    // NOTE(khvorov) Init
+    {
+        llvm::Target& x8664Target = llvm::getTheX86_64Target();
+        llvm::TargetRegistry::RegisterTarget(x8664Target, "x86-64", "64-bit X86: EM64T and AMD64", "X86", LLVMX8664MatchProc, true);
+        llvm::TargetRegistry::RegisterTargetMachine(x8664Target, LLVMX86TargetMachineProc);
+
+        llvm::PassRegistry& PR = *llvm::PassRegistry::getPassRegistry();
+        llvm::initializeX86LowerAMXIntrinsicsLegacyPassPass(PR);
+        llvm::initializeX86LowerAMXTypeLegacyPassPass(PR);
+        llvm::initializeX86PreAMXConfigPassPass(PR);
+        llvm::initializeX86PreTileConfigPass(PR);
+        llvm::initializeGlobalISel(PR);
+        llvm::initializeWinEHStatePassPass(PR);
+        llvm::initializeFixupBWInstPassPass(PR);
+        llvm::initializeEvexToVexInstPassPass(PR);
+        llvm::initializeFixupLEAPassPass(PR);
+        llvm::initializeFPSPass(PR);
+        llvm::initializeX86FixupSetCCPassPass(PR);
+        llvm::initializeX86CallFrameOptimizationPass(PR);
+        llvm::initializeX86CmovConverterPassPass(PR);
+        llvm::initializeX86TileConfigPass(PR);
+        llvm::initializeX86FastPreTileConfigPass(PR);
+        llvm::initializeX86FastTileConfigPass(PR);
+        llvm::initializeX86KCFIPass(PR);
+        llvm::initializeX86LowerTileCopyPass(PR);
+        llvm::initializeX86ExpandPseudoPass(PR);
+        llvm::initializeX86ExecutionDomainFixPass(PR);
+        llvm::initializeX86DomainReassignmentPass(PR);
+        llvm::initializeX86AvoidSFBPassPass(PR);
+        llvm::initializeX86AvoidTrailingCallPassPass(PR);
+        llvm::initializeX86SpeculativeLoadHardeningPassPass(PR);
+        llvm::initializeX86SpeculativeExecutionSideEffectSuppressionPass(PR);
+        llvm::initializeX86FlagsCopyLoweringPassPass(PR);
+        llvm::initializeX86LoadValueInjectionLoadHardeningPassPass(PR);
+        llvm::initializeX86LoadValueInjectionRetHardeningPassPass(PR);
+        llvm::initializeX86OptimizeLEAPassPass(PR);
+        llvm::initializeX86PartialReductionPass(PR);
+        llvm::initializePseudoProbeInserterPass(PR);
+        llvm::initializeX86ReturnThunksPass(PR);
+        llvm::initializeX86DAGToDAGISelPass(PR);
+    }
+
     const char* Argv0 = argv[0];
     SmallVector<const char*, 256> Args(argv, argv + argc);
     ArrayRef<const char*> Argv = makeArrayRef(Args).slice(1);
-
-    ensureSufficientStack();
 
     std::unique_ptr<CompilerInstance> Clang(new CompilerInstance());
     IntrusiveRefCntPtr<DiagnosticIDs> DiagID(new DiagnosticIDs());
--- clang_src/clang_tools_driver_driver.cpp
+++ clang_src/clang_tools_driver_driver.cpp
@@ -1,11 +1,5 @@
-#include "llvm_lib_Target_X86_TargetInfo_X86TargetInfo.h"
-#include "llvm_lib_Target_X86_X86TargetMachine.h"
-#include "llvm_lib_Target_X86_X86.h"
-
-#include "llvm_include_llvm_MC_TargetRegistry.h"
-#include "llvm_include_llvm_InitializePasses.h"
-
-#include "clang_include_clang_Driver_Driver.h"
+#include <assert.h>
+#include <stdint.h>
 
 // clang-format off
 #define mdc_STR(x) (mdc_Str) { x, mdc_strlen(x) }
@@ -62,63 +56,10 @@ mdc_streq(mdc_Str str1, mdc_Str str2) {
     return result;
 }
 
-static bool
-LLVMX8664MatchProc(llvm::Triple::ArchType archType) {
-    bool result = archType == llvm::Triple::x86_64;
-    return result;
-}
-
-static llvm::TargetMachine*
-LLVMX86TargetMachineProc(const llvm::Target& T, const llvm::Triple& TT, llvm::StringRef CPU, llvm::StringRef FS, const llvm::TargetOptions& Options, std::optional<llvm::Reloc::Model> RM, std::optional<llvm::CodeModel::Model> CM, llvm::CodeGenOpt::Level OL, bool JIT) {
-    return new llvm::X86TargetMachine(T, TT, CPU, FS, Options, RM, CM, OL, JIT);
-}
-
 extern int cc1_main(int argc, char** argv);
 
 extern "C" int
 clang_main(int argc, char** argv) {
-    // NOTE(khvorov) Init
-    {
-        llvm::Target& x8664Target = llvm::getTheX86_64Target();
-        llvm::TargetRegistry::RegisterTarget(x8664Target, "x86-64", "64-bit X86: EM64T and AMD64", "X86", LLVMX8664MatchProc, true);
-        llvm::TargetRegistry::RegisterTargetMachine(x8664Target, LLVMX86TargetMachineProc);
-
-        llvm::PassRegistry& PR = *llvm::PassRegistry::getPassRegistry();
-        llvm::initializeX86LowerAMXIntrinsicsLegacyPassPass(PR);
-        llvm::initializeX86LowerAMXTypeLegacyPassPass(PR);
-        llvm::initializeX86PreAMXConfigPassPass(PR);
-        llvm::initializeX86PreTileConfigPass(PR);
-        llvm::initializeGlobalISel(PR);
-        llvm::initializeWinEHStatePassPass(PR);
-        llvm::initializeFixupBWInstPassPass(PR);
-        llvm::initializeEvexToVexInstPassPass(PR);
-        llvm::initializeFixupLEAPassPass(PR);
-        llvm::initializeFPSPass(PR);
-        llvm::initializeX86FixupSetCCPassPass(PR);
-        llvm::initializeX86CallFrameOptimizationPass(PR);
-        llvm::initializeX86CmovConverterPassPass(PR);
-        llvm::initializeX86TileConfigPass(PR);
-        llvm::initializeX86FastPreTileConfigPass(PR);
-        llvm::initializeX86FastTileConfigPass(PR);
-        llvm::initializeX86KCFIPass(PR);
-        llvm::initializeX86LowerTileCopyPass(PR);
-        llvm::initializeX86ExpandPseudoPass(PR);
-        llvm::initializeX86ExecutionDomainFixPass(PR);
-        llvm::initializeX86DomainReassignmentPass(PR);
-        llvm::initializeX86AvoidSFBPassPass(PR);
-        llvm::initializeX86AvoidTrailingCallPassPass(PR);
-        llvm::initializeX86SpeculativeLoadHardeningPassPass(PR);
-        llvm::initializeX86SpeculativeExecutionSideEffectSuppressionPass(PR);
-        llvm::initializeX86FlagsCopyLoweringPassPass(PR);
-        llvm::initializeX86LoadValueInjectionLoadHardeningPassPass(PR);
-        llvm::initializeX86LoadValueInjectionRetHardeningPassPass(PR);
-        llvm::initializeX86OptimizeLEAPassPass(PR);
-        llvm::initializeX86PartialReductionPass(PR);
-        llvm::initializePseudoProbeInserterPass(PR);
-        llvm::initializeX86ReturnThunksPass(PR);
-        llvm::initializeX86DAGToDAGISelPass(PR);
-    }
-
     // NOTE(khvorov) Only support generating objs via the cc1 tool
     int result = 0;
     {
