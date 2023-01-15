3941d12535dc67d5a5e687fd632e84c97ab8d7a6
Arseniy Khvorov <khvorov45@gmail.com>
2023-01-15 14:14:38 +1100

    only do the cc1 tool

--- tests.c
+++ tests.c
@@ -31,11 +31,44 @@ runTestForProgram(prb_Arena* arena, i32 counter, prb_Str program) {
         prb_assert(prb_writeEntireFile(arena, programFilepath, program.ptr, program.len));
     }
 
-    prb_Str outname = prb_fmt(arena, "prog%d.exe", counter);
-    prb_Str outpath = prb_pathJoin(arena, globalTestDir, outname);
-    prb_Str cmd = prb_fmt(arena, "%.*s -I%.*s %.*s -o %.*s", prb_LIT(globalMyClangExe), prb_LIT(globalMyClangHeaders), prb_LIT(programFilepath), prb_LIT(outpath));
-    execCmd(arena, cmd);
-    execCmd(arena, outpath);
+    prb_Str outpathObj = {};
+    {
+        prb_Str outnameObj = prb_fmt(arena, "prog%d.obj", counter);
+        outpathObj = prb_pathJoin(arena, globalTestDir, outnameObj);
+        prb_assert(prb_removePathIfExists(arena, outpathObj));
+
+        prb_Str cmdObj = prb_fmt(
+            arena,
+            "%.*s -cc1 -triple x86_64-unknown-linux-gnu -emit-obj -mrelax-all -disable-free -clear-ast-before-backend -main-file-name %.*s "
+            "-mrelocation-model pic -pic-level 2 -pic-is-pie -mframe-pointer=all -fmath-errno -ffp-contract=on -fno-rounding-math "
+            "-mconstructor-aliases -funwind-tables=2 -target-cpu x86-64 -tune-cpu generic -mllvm -treat-scalable-fixed-error-as-warning "
+            "-debugger-tuning=gdb -fcoverage-compilation-dir=%.*s "
+            "-I %.*s -internal-isystem /usr/local/include "
+            "-internal-isystem /usr/lib/gcc/x86_64-linux-gnu/11/../../../../x86_64-linux-gnu/include "
+            "-internal-externc-isystem /usr/include/x86_64-linux-gnu -internal-externc-isystem /include "
+            "-internal-externc-isystem /usr/include -fdebug-compilation-dir=%.*s -ferror-limit 19 "
+            "-fgnuc-version=4.2.1 -faddrsig -D__GCC_HAVE_DWARF2_CFI_ASM=1 "
+            "-o %.*s -x c %.*s",
+            prb_LIT(globalMyClangExe),
+            prb_LIT(programFilepath),
+            prb_LIT(globalTestDir),
+            prb_LIT(globalMyClangHeaders),
+            prb_LIT(globalTestDir),
+            prb_LIT(outpathObj),
+            prb_LIT(programFilepath)
+        );
+        execCmd(arena, cmdObj);
+    }
+
+    {
+        prb_Str outnameExe = prb_fmt(arena, "prog%d.exe", counter);
+        prb_Str outpathExe = prb_pathJoin(arena, globalTestDir, outnameExe);
+        prb_assert(prb_removePathIfExists(arena, outpathExe));
+
+        prb_Str cmdExe = prb_fmt(arena, "clang %.*s -o %.*s", prb_LIT(outpathObj), prb_LIT(outpathExe));
+        execCmd(arena, cmdExe);
+        execCmd(arena, outpathExe);
+    }
 
     prb_endTempMemory(temp);
 }
